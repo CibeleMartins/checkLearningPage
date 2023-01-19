a6d096ac6cf8ebab174fd6c1bdcbd50c3b5a2cc1
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-19 19:16:51 -0300

    ➕ build: adicionou dependencia de animações de texto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TeximateModule } from 'ngx-teximate';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -12,7 +14,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TeximateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
