056cbf98226714e611cac41aebafef7c1a597b19
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-21 17:04:32 -0300

    🚧 feat: testando breakpoints

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,36 +2,37 @@ import { Component, OnInit } from '@angular/core';
 // import { fadeInDown, fadeInLeft } from 'ng-animate';
 // import { TextAnimation } from 'ngx-teximate';
 
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  poster: string = '../../../../assets/poster.svg';
+  checkList: string = '';
+  learnerMethod: string = '';
 
-  poster: string = "../../../../assets/poster.svg";
-  checkList: string = "";
-  learnerMethod: string = "";
+  sizeTitleInitialContent = '50%';
+  size2 = '100px';
 
-  sizeTitleInitialContent = false;
-
-  constructor(private breakpointService: BreakpointObserver) {
-    
-  }
+  constructor(private breakpointService: BreakpointObserver) {}
 
   ngOnInit(): void {
+    this.breakpointService
+      .observe([Breakpoints.Tablet, Breakpoints.XSmall])
+      .subscribe((result) => {
+        this.sizeTitleInitialContent = '50%';
+        this.size2 = '100px';
 
-    this.breakpointService.observe(Breakpoints.Tablet).subscribe((result)=> {
+        if (result.breakpoints[Breakpoints.Tablet]) {
+          this.sizeTitleInitialContent = '0%';
+        }
 
-      this. sizeTitleInitialContent = false;
-
-      if (result.matches) {
-
-        this.sizeTitleInitialContent = true;
-      }
-    })
+        if (result.breakpoints[Breakpoints.XSmall]) {
+          this.size2 = '50px';
+        }
+      });
   }
-
 }
