15a5b8fb02da29db4660dbe44d0ec89fc980abb8
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-22 17:05:36 -0300

    🚧 feat: implementou animações e removeu "stylesheet" de uma tag link do angular material

--- src/app/components/layout-home/layout-home.component.ts
+++ src/app/components/layout-home/layout-home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Output, Input } from '@angular/core';
 
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-
 @Component({
   selector: 'app-layout-home',
   templateUrl: './layout-home.component.html',
@@ -10,20 +8,13 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 export class LayoutHomeComponent implements OnInit {
 
   @Input() image: string | undefined;
-  @Input() image2: string | undefined;
 
   fluidSizePoster = false;
 
-  constructor(private breakpointService: BreakpointObserver) { }
+  constructor() { }
 
   ngOnInit(): void {
 
-    this.breakpointService.observe([Breakpoints.Handset, Breakpoints.Tablet]).subscribe((result)=> {
-
-      if(result.breakpoints[Breakpoints.Tablet]) {
-          this.fluidSizePoster = true;
-      }
-    })
   }
 
 }
