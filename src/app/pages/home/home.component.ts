f7964c56d618d6f24e124b982e2aaab3a692abc0
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-23 10:55:15 -0300

    🩹 feat: corrigiu o problema do título do login não estar aparecendo quando a tag <h1> é empregada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as Aos from 'aos';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
