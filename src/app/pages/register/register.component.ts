dceb70491a86c96f5889fe311d42e511b987837b
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-25 16:29:32 -0300

    🚧 feat: add validators no forms de cadastro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,56 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  EmailValidator,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   registerForm: FormGroup;
   signupForm!: FormGroup;
   viewSnackbar: boolean = false;
   messageSnackBar!: string;
   warningIcon!: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      'completeName': new FormControl('', [Validators.required, Validators.minLength(10), this.completeNameValidator.bind(this)], null),
-      'email': new FormControl('', [Validators.required, Validators.email, this.emailValidator.bind(this)], null),
-      'password': new FormControl('', [Validators.required, Validators.minLength(6)], null),
-      'confirmationPassword': new FormControl('', [Validators.required], null),
-    })
+      completeName: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          this.completeNameValidator.bind(this),
+        ],
+        null
+      ),
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email, this.emailValidator.bind(this)],
+        null
+      ),
+      password: new FormControl(
+        '',
+        [Validators.required, Validators.minLength(6), this.passwordValidator.bind(this)],
+        null
+      ),
+      confirmationPassword: new FormControl('', [Validators.required], null),
+    });
   }
 
-  completeNameValidator(control: FormControl): {[s: string]: boolean} {
-
-    setTimeout(()=>{
-      if(control.errors) {
+  completeNameValidator(control: FormControl): { [s: string]: boolean } {
+   const timeout = setTimeout(() => {
+      if (control.value && control.errors) {
         this.viewSnackbar = !this.viewSnackbar;
         this.messageSnackBar = 'O nome completo muito pequeno.';
         this.warningIcon = '../../../assets//warningIcon.png';
         // return{'minLengthPasswordError': true}
       }
-     
-    }, 5000)
 
-    
-    console.log(control)
-   
+    }, 3000);
+
+ 
+    // console.log(control);
 
     return null;
   }
 
   emailValidator(control: FormControl): { [s: string]: boolean } {
-
     // console.log(control)
-    if (
-      control.value && control.errors &&
-     control.errors['email']
-    ) {
+    if (control.value && control.errors && control.errors['email']) {
       this.viewSnackbar = !this.viewSnackbar;
       this.messageSnackBar = 'E-mail inválido.';
       this.warningIcon = '../../../assets//warningIcon.png';
@@ -63,7 +77,7 @@ export class RegisterComponent implements OnInit {
 
   passwordValidator(control: FormControl): { [s: string]: boolean } {
     setTimeout(() => {
-      if (control.value && control.value.length < 6) {
+      if (control.value && control.errors) {
         this.viewSnackbar = !this.viewSnackbar;
         this.messageSnackBar = 'Senha muito pequena.';
         this.warningIcon = '../../../assets//warningIcon.png';
@@ -72,5 +86,4 @@ export class RegisterComponent implements OnInit {
 
     return null;
   }
-
 }
